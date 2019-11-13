e66255e892012b014b3a4e91dedf7e4de84af079
Keith Kim <karmakaze@users.noreply.github.com>
2019-11-13 17:26:20 -0500

    Updates from PR review

--- lib/shopify_api/resources/fulfillment_order.rb
+++ lib/shopify_api/resources/fulfillment_order.rb
@@ -14,12 +14,20 @@ module ShopifyAPI
     end
 
     def move(new_location_id:)
-      body = { fulfillment_order: { new_location_id: new_location_id } }
-      load_values(post(:move, body, only_id))
+      body = { fulfillment_order: { new_location_id: new_location_id } }.to_json
+      keyed_fos = load_keyed_attributes_from_response(post(:move, {}, body))
+      if keyed_fos&.fetch('original_fulfillment_order', nil)&.attributes
+        load(keyed_fos['original_fulfillment_order'].attributes, false, true)
+      end
+      keyed_fos
     end
 
     def cancel
-      load_values(post(:cancel, {}, only_id))
+      keyed_fos = load_keyed_attributes_from_response(post(:cancel, {}, only_id))
+      if keyed_fos&.fetch('fulfillment_order', nil)&.attributes
+        load(keyed_fos['fulfillment_order'].attributes, false, true)
+      end
+      keyed_fos
     end
 
     def close
@@ -28,7 +36,7 @@ module ShopifyAPI
 
     private
 
-    def load_values(response)
+    def load_keyed_attributes_from_response(response)
       return load_attributes_from_response(response) if response.code != '200'
 
       keyed_fulfillments = ActiveSupport::JSON.decode(response.body)
