860b2f2315441a3e1936023f53f01b01fd8d29b2
Mike Schwab <mike.schwab@gmail.com>
2019-11-21 18:15:21 -0500

    alternative heredoc syntax

--- lib/active_resource/detailed_log_subscriber.rb
+++ lib/active_resource/detailed_log_subscriber.rb
@@ -27,7 +27,7 @@ module ActiveResource
       payload[:response].each do |header_name, header_value|
         case header_name.downcase
         when 'x-shopify-api-deprecated-reason'
-          warning_message = <<~MSG
+          warning_message = <<-MSG
             [DEPRECATED] ShopifyAPI made a call to #{payload[:method].upcase} #{payload[:path]}, and this call made
             use of a deprecated endpoint, behaviour, or parameter. See #{header_value} for more details.
           MSG
